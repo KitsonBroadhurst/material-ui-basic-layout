c2bf151c4d6cc471743a9ad383bc958ee6d880d5
Kitson <kitson.broadhurst@gmail.com>
2020-02-10 17:06:12 +0000

    Add a baseline theme file and wrapp our App component in a ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from './theme'
 
-function App() {
-  return (
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <div>
       <p>A bare bones application!</p>
     </div>
-  );
-}
-
+  </ThemeProvider>
+)
 export default App
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createMuiTheme }  from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { 500: '#467fcf' },
+  },
+})
+
+export default theme
